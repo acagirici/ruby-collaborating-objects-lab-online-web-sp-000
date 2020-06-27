8df2f81e4abdcac3b91faa5608e9e01eeb9fabe9
Arif Cagirici <acagirici23@gmail.com>
2020-06-27 19:54:55 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,10 +1,12 @@
 class MP3Importer
-  attr_accessor :file 
+  attr_accessor :path 
   
-  def initialize(file)
-    @file = file
+  def initialize(path)
+    @path = path
   end
   
+  def files 
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.
   def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
   end
