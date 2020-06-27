ec05cde53ee8b7dbff0b1850ef4778990f5effee
Arif Cagirici <acagirici23@gmail.com>
2020-06-27 19:50:34 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,7 +1,11 @@
 class MP3Importer
+  attr_accessor :file 
   
+  def initialize
+    @file = file
+  end
   
   def import(list_of_filenames)
-    list_of_filenames.each{ |filename| song.new_by_filename(filename) }
+    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
   end
 end
