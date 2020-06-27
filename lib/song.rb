60a0308606ddb656fc3b67d99f1fe61f8f09b586
Arif Cagirici <acagirici23@gmail.com>
2020-06-27 19:14:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,11 @@
 class Song 
-  attr_accessor :name 
+  attr_accessor :name, :artist 
   
   @@all = []
   
   def initialize(name)
     @name = name
+    @artist = artist
     @@all << self
   end
   
