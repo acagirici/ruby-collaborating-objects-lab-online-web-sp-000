f525d2138a4a8f9701e82b4c5a8ae0f5a6273825
Arif Cagirici <acagirici23@gmail.com>
2020-06-27 20:06:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,11 @@ class Song
     @name = name
     @@all << self
   end
-
+  
+  def self
+    @@all
+  end
+  
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
